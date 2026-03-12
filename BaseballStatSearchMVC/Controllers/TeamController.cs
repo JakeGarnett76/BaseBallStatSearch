5279912e6a8b1b6de93a71dc8244ecb68c106862
JakeGarnett76 <jakegarnett6171@gmail.com>
2026-03-11 23:55:58 -0500

    Add Team Controller and views

---
+++ BaseballStatSearchMVC/Controllers/TeamController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace BaseballStatSearchMVC.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: /Team/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /Team/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
